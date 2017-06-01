f31143e0c4cb33dc573644c70104eba5894fe774
Philip Domingo <philipdomingo@outlook.com>
2017-06-02 03:00:48 +0800

    Added OrderFood scenario

--- app.js
+++ app.js
@@ -1,47 +1,95 @@
+'use strict';
+
 // Load .env file 
 require('dotenv-extended').load();
 
-var builder = require('botbuilder');
-var restify = require('restify');
-var Restaurant = require('./restaurant');
+const builder = require('botbuilder');
+const restify = require('restify');
+const Restaurant = require('./restaurant');
 
 // setup server
-var server = restify.createServer();
-server.listen(process.env.port || process.env.PORT || 3978, function () {
+const server = restify.createServer();
+server.listen(process.env.port || process.env.PORT || 3978, () => {
     console.log('%s listening to %s', server.name, server.url);
 });
 
 // connector for bot framework
-var connector = new builder.ChatConnector({
+const connector = new builder.ChatConnector({
     appId: process.env.MICROSOFT_APP_ID,
     appPassword: process.env.MICROSOFT_APP_PASSWORD
 });
 server.post('/api/messages', connector.listen());
 
-var bot = new builder.UniversalBot(connector, function (session) {
-    session.send('Sorry, I did not understand your message \'%s\'. You can type \'Help\' for assistance', session.message.text);
+const bot = new builder.UniversalBot(connector, (session) => {
+    session.send('Sorry, I did not understand your message \'%s\'', session.message.text);
 });
 
 // See https://www.luis.ai to create your own service
-var recognizer = new builder.LuisRecognizer(process.env.LUIS_MODEL_URL);
+const recognizer = new builder.LuisRecognizer(process.env.LUIS_MODEL_URL);
 bot.recognizer(recognizer);
 
 bot.dialog('OrderFood', [
-    function(session, args, next){
-        session.send('Order Food Dialog');
+    (session, args, next) => {
+        const foodName = builder.EntityRecognizer.findEntity(args.intent.entities, 'foodName');
+        const foodSize = builder.EntityRecognizer.findEntity(args.intent.entities, 'foodSize');
+        const restaurantName = builder.EntityRecognizer.findEntity(args.intent.entities, 'restaurantName');
+
+        let foodOrder = session.dialogData.foodOrder = {
+            name: foodName ? foodName.entity : null,
+            size: foodSize ? foodSize.entity : null,
+            restaurant: restaurantName ? restaurantName.entity : null
+        };
+
+        if(foodOrder.name) {
+            next();
+        } else {
+            builder.Prompts.text(session, 'What food would you like to order?');
+        }
+    },
+    (session, results, next) => {
+        let foodOrder = session.dialogData.foodOrder;
+        if(results.response) {
+            foodOrder.name = results.response;
+        } 
+
+        if(foodOrder.size) {
+            next();
+        } else {
+            builder.Prompts.choice(session, 'What size would you want?', ['regular', 'large', 'jumbo']);
+        }
+    },
+    (session, results, next) => {
+        let foodOrder = session.dialogData.foodOrder;
+        if(results.response) {
+            foodOrder.size = results.response;
+        }
+
+        if(foodOrder.restaurant) {
+            next();
+        } else {
+            builder.Prompts.text(session, 'Where would you want to order this food?');
+        }
+    },
+    (session, results) => {
+        let foodOrder = session.dialogData.foodOrder;
+        if(results.response) {
+            foodOrder.restaurant = results.response;
+        }
+        
+        let message = 'I will now order your ' + (foodOrder.size.entity ? foodOrder.size.entity : foodSize.entity) + ' ' + 
+                        foodOrder.name + ' at ' + foodOrder.restaurant;
+
+        session.send(message);
         session.endDialog();
     }
-]).triggerAction({
-    matches: 'OrderFood',
-    onInterrupted: function(session){
-        session.send('OrderFood on interrupted');
-    }
-});
+}).cancelAction('cancelOrder', "Order canceled.", {
+    matches: /^(cancel|nevermind)/i,
+    confirmPrompt: "Are you sure?"
+});;
 
 bot.dialog('SearchRestaurant', [
-    function(session, args, next){
-
-        var geographyEntity = builder.EntityRecognizer.findEntity(args.intent.entities, 'PhilippinesGeography');
+    (session, args, next) => {
+        const geographyEntity = builder.EntityRecognizer.findEntity(args.intent.entities, 'PhilippinesGeography');
 
         if(geographyEntity){
             next({ response: geographyEntity.entity });
@@ -49,7 +97,7 @@ bot.dialog('SearchRestaurant', [
             builder.Prompts.text(session, 'Where?');
         }
     },
-    function (session, result){
+    (session, result) => {
         var location = result.response;
 
         var message = 'Just a second, I\'m currently looking for restaurants around %s...';
@@ -66,12 +114,7 @@ bot.dialog('SearchRestaurant', [
         });
 
     }
-]).triggerAction({
-    matches: 'SearchRestaurant',
-    onInterrupted: function(session){
-        session.send('Search Restaurant OnInterrupted');
-    }
-});
+]);
 
 function restaurantAsAttachment(restaurant) {
     return new builder.HeroCard()
@@ -82,7 +125,7 @@ function restaurantAsAttachment(restaurant) {
                           new builder.CardAction()
                                      .title('View in Google Maps')
                                      .type('openUrl')
-                                     .value('https://www.google.com.ph/maps/search/' + encodeURIComponent(restaurant.name) 
-                                        + ' in ' + encodeURIComponent(restaurant.location))
+                                     .value('https://www.google.com.ph/maps/search/' + 
+                                        encodeURIComponent(restaurant.name) + ' in ' + encodeURIComponent(restaurant.location))
                       ]);
 }
